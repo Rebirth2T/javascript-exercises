c73ac2eb526193ff53a1f3834f3106de151a3a32
Jan <jan@resonyte.com>
2024-09-21 18:36:58 +0100

    Finishing exercises 1 to 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...filters) {
+    return array.filter(item => !filters.includes(item));
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+
+    } else if (a < 0 || b < 0) {
+        return "ERROR";
+    } else {
+        const min = Math.min(a, b);
+        const max = Math.max(a, b);
+        for (let i = min; i <= max; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    
+
+
 
 };
 
