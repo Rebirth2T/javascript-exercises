9f8820de2f626c4657dd83bec264c3bd8f75c184
Jan <jan@resonyte.com>
2024-10-05 21:24:43 +0100

    first two exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(input) {
+	return input.reduce((accum, curr) => {
+    return accum + curr;
+  },0);
 };
 
-const multiply = function() {
-
+const multiply = function(input) {
+  return input.reduce((accum, cur) => accum*cur);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let output = 1;
+  for (let i = 1; i <= a; i++) {
+    output = output * i;
+  }
+  return output;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let str_length = string.length;
 
+    // Loop through the first half of the string
+    for (let i = 0; i < Math.floor(str_length / 2); i++) {
+        // Compare characters from the start and the end
+        if (string[i] !== string[str_length - 1 - i]) {
+            return false;
+        }
+    }
+    return true; // If no mismatches, it is a palindrome
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
