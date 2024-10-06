6f7bbf026e6ac8aa0fb03243caa9de2fc67b7591
Jan <jan@resonyte.com>
2024-10-06 12:42:55 +0100

    commits of remaining exercises, excl ex 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    x = parseInt(x)
+    let fibonacci_array = [];
+    for (let i = 0; i < x; i++) {
+        if (i < 2) {
+            fibonacci_array.push(1)
+        }
+        else {
+            fibonacci_array.push(fibonacci_array[i-2] + fibonacci_array[i-1]);
+        }
+    }
+    if (x === 0) {
+        return 0
+    }
+    else {
+        return fibonacci_array[x-1]
+    }
 };
 
 // Do not edit below this line
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let bookArray = [];
 
+    books.forEach(item => {
+        bookArray.push(item.title);
+    });
+    
+    return bookArray
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
